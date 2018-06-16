f5000d4dcdeb25eb2dbacbe8da5a0156484f24dc
NerOcrO <nerocro@gmail.com>
2018-06-16 11:10:56 +0200

    variable name changed

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -21,7 +21,7 @@ const chatbot = {
         path: '/',
         async handler(request) {
           const swansonQuotes = await axios.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
-          const result = await request.mongo.db.collection('chatbot').findOne({ userId: request.payload.user_id, 'thread.id': request.state.id })
+          let result = await request.mongo.db.collection('chatbot').findOne({ userId: request.payload.user_id, 'thread.id': request.state.id })
 
           if (result) {
             const values = {
@@ -45,9 +45,9 @@ const chatbot = {
             }
           }
 
-          const result2 = await request.mongo.db.collection('chatbot').findOne({ userId: request.payload.user_id })
+          result = await request.mongo.db.collection('chatbot').findOne({ userId: request.payload.user_id })
 
-          if (result2) {
+          if (result) {
             const values = {
               thread: {
                 id: request.state.id,
